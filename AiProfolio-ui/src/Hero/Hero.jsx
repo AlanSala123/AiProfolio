181a02355df8450be1f7c70b2cee24cb8fce27ca
Luis Bravo <lbravo@lbravo-ltmxcuw.internal.salesforce.com>
2023-07-12 10:13:39 -0700

    init navbar and hero

---
+++ AiProfolio-ui/src/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import './Hero.css'
+
+export default function Hero(){
+
+    return(
+    <div id="hero">
+        <div className='title'> 
+        <h2 id="ai">Ai</h2>
+        <h1>Profolio</h1>
+        </div>
+    </div>
+        )
+
+}
