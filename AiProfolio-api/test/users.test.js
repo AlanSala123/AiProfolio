2eb87c52d1757a89bf53166a9ed579210c8aa8b0
Luis Bravo <lbravo@lbravo-ltmxcuw.internal.salesforce.com>
2023-07-14 09:38:40 -0700

    userModel and testing 92.06% coverage

--- AiProfolio-api/test/users.test.js
+++ AiProfolio-api/test/users.test.js
@@ -1,8 +1,8 @@
 const User = require('../models/user.js')
-const { NotFoundError, InternalServerError, FieldValidationError } = require('../utilities/error')
+const { NotFoundError, InternalServerError, FieldValidationError, InvalidCredentialsError } = require('../utilities/error')
 
 
-describe("Fetch", ()=>{
+describe("Fetch User", ()=>{
 
     test('fetch by id should return user for valid user id', async()=>{
          const validUserId = 'test-id'
@@ -15,7 +15,7 @@ describe("Fetch", ()=>{
         })
      })
     
-    test('fetch by id should return Not Found Error for invalid user id', async () => {
+    test('fetch by id should throw Not Found Error for invalid user id', async () => {
         const invalidUserId = 'test-fake-id'
             try {
                 await User.fetch("id", invalidUserId)
@@ -25,7 +25,7 @@ describe("Fetch", ()=>{
             }
     })
 
-    test('fetch by email should return user for valid user email', async()=>{
+    test('fetch by email should return user for valid user email', async ()=>{
         const validUserEmail = 'test-email@test.com'
         const result = await User.fetch("email",validUserEmail)
         expect(result).toStrictEqual({
@@ -36,13 +36,13 @@ describe("Fetch", ()=>{
        })
     })
    
-   test('fetch by email should return Not Found Error for invalid user email', async () => {
+   test('fetch by email should throw Not Found Error for invalid user email', async () => {
        const invalidUserEmail = 'test-fake-id'
            try {
-               await User.fetch("email", invalidUserEmail)
+                await User.fetch("email", invalidUserEmail)
                fail()
            } catch (error) {
-               expect(error instanceof NotFoundError).toBeTruthy()
+            expect(error instanceof NotFoundError).toBeTruthy()
            }
    })
 
@@ -54,9 +54,278 @@ describe("Fetch", ()=>{
         } catch (error) {
             expect(error instanceof FieldValidationError).toBeTruthy()
         }
+})  
+
 })
 
-    //describe("GenerateAuthToken")
+describe("Token Handling", ()=>{
+
+    test("Generate token should return a valid token which can be decoded", ()=>{
+        const token = User.generateAuthToken({
+            id : 'test-id', 
+            password : 'test-password',
+            first_name : 'test-first_name',
+            email : 'test-email@test.com'
+        })
+        const user = User.verifyToken(token)
+        expect({
+            id: user.id,
+            first_name : user.first_name,
+            email : user.email
+        }).toStrictEqual({
+            id : 'test-id', 
+            first_name : 'test-first_name',
+            email : 'test-email@test.com'
+        })
+    })
+
+    test("Token not of type string should throw a InvalidCredentials Error",()=>{
+        try {
+            const fakeToken = 0
+            User.verifyToken(fakeToken)
+            fail()
+        } catch (error) {
+            expect(error instanceof InvalidCredentialsError).toBeTruthy()
+        }
+    })
+
+    test("Expired token should throw an InvalidCredentials Error", ()=>{
+        try {
+            const expiredToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ
+            pZCI6InRlc3QtaWQiLCJmaXJzdF9uYW1lIjoidGVzdC1maXJzdF9uYW1lIiwiZ
+            W1haWwiOiJ0ZXN0LWVtYWlsQHRlc3QuY29tIiwiaWF0IjoxNjg5Mjg4NjUyLCJl
+            eHAiOjE2ODkyODg2NTV9.cAEy3S4OVUfbp4Crsl8FFnYiJOiiqICpL263CVE_TqM`
+            
+            User.verifyToken(expiredToken)
+        } catch (error) {
+           expect(error instanceof InvalidCredentialsError).toBeTruthy() 
+        }
+    })
+
+})
+
+describe("User Utilities", ()=>{
+    test("validateEmail should return true if email meets criteria", ()=>{
+        const validEmail = 'luisbravo@test.com'
+        const result = User.validateEmail(validEmail)
+        expect(result).toBe(true)
+    })
+
+    test("validateEmail should return false if email does not meets criteria (No Uppercase)", ()=>{
+        const invalidEmail = 'luisbravo123'
+        try {
+            User.validateEmail(invalidEmail)
+            fail()
+        } catch (error) {
+            expect(error instanceof FieldValidationError).toBeTruthy()
+        }
+    })
+
+    test("validateEmail should return false if email does not meets criteria (No lowercase)", ()=>{
+        const invalidEmail = 'LUISBRAVO123'
+        try {
+            User.validateEmail(invalidEmail)
+            fail()
+        } catch (error) {
+            expect(error instanceof FieldValidationError).toBeTruthy()
+        }
+    })
+
+    test("validateEmail should return false if email does not meets criteria (No digit)", ()=>{
+        const invalidEmail = 'Luisbravo'
+        try {
+            User.validateEmail(invalidEmail)
+            fail()
+        } catch (error) {
+            expect(error instanceof FieldValidationError).toBeTruthy()
+        }
+    })
+
+    test("validateEmail should return false if email does not meets criteria (Not 8 chars)", ()=>{
+        const invalidEmail = 'Lu123'
+        try {
+            User.validateEmail(invalidEmail)
+            fail()
+        } catch (error) {
+            expect(error instanceof FieldValidationError).toBeTruthy()
+        }
+    })
+
+    test("validatePassword should return true if password meets criteria", ()=>{
+        const validPassword = 'Password12345'
+        const result = User.validatePassword(validPassword)
+        expect(result).toBe(true)
+    })
+
+    test("validatePassword should throw FieldValidationError if password does not meets criteria", ()=>{
+        const invalidPassword = 'password'
+        try {
+            User.validatePassword(invalidPassword)
+            fail()
+        } catch (error) {
+            expect(error instanceof FieldValidationError).toBeTruthy()
+        }  
+    })
+})
+
+describe("Create", ()=>{
+
+    beforeAll(async () => {
+        await User.delete('creation@test.com')
+    });
+
+    test("Create User With Valid Inputs should successfully create a user", async ()=>{
+        const password = "Password123"
+        const email = "creation@test.com"
+        const first_name = "test"
+        const createdUser = await User.create({password, email, first_name})
+        expect(createdUser.email).toStrictEqual(email)
+
+    })
+
+    test("Create User With invalid Inputs(password) should throw FieldValidationError", async ()=>{
+        const password = "1"
+        const email = "creation@test.com"
+        const first_name = "test"
+        try {
+            await User.create({password, email, first_name})
+            fail()
+        } catch (error) {
+            expect(error instanceof FieldValidationError).toBeTruthy()
+        }
+    })
+
+    test("Create User With invalid Inputs(email) should throw FieldValidationError", async ()=>{
+        const password = "Password123"
+        const email = "1"
+        const first_name = "test"
+        try {
+            await User.create({password, email, first_name})
+            fail()
+        } catch (error) {
+            expect(error instanceof FieldValidationError).toBeTruthy()
+        }
+    })
+
     
-
 })
+
+describe("Login", ()=>{
+
+    test("Created User Can Log in with valid credentials", async ()=>{
+        const email = "creation@test.com"
+        const password = "Password123"
+        const loginForm = {
+            email: email,
+            password: password
+        }
+        const {user, token} = await User.login(loginForm, "")
+        expect(user.email).toStrictEqual(email)
+    })
+
+    test("Login fails with non existing email address and throws NotFoundError", async ()=>{
+        const email = "fake@test.com"
+        const password = "Password123"
+        const loginForm = {
+            email: email,
+            password: password
+        }
+        try {
+            await User.login(loginForm, "")
+            fail()
+        } catch (error) {
+            expect(error instanceof NotFoundError).toBeTruthy()
+        }
+        
+    })
+    test("Login fails with invalid password and throws NotFoundError", async ()=>{
+        const email = "creation@test.com"
+        const password = "invalid"
+        const loginForm = {
+            email: email,
+            password: password
+        }
+        try {
+            await User.login(loginForm, "")
+            fail()
+        } catch (error) {
+            expect(error instanceof InvalidCredentialsError).toBeTruthy()
+        }
+    })
+})
+
+describe("Register", ()=>{
+    test("Register should create a new user if valid", async ()=>{
+        const registrationForm = {
+            email: "test2@test.com",
+            password:"validPassword123",
+            first_name:"test-2"
+        }
+        const {user, token} = await User.register(registrationForm)
+        expect(user.email == registrationForm.email && token).toBeTruthy()
+    })
+
+    test("User can be deleted", async ()=>{
+        const email = 'test2@test.com'
+        await User.delete(email)
+        try {
+            await User.fetch("email", email)
+        } catch (error) {
+            expect(error instanceof NotFoundError).toBeTruthy()
+        }
+    })
+})
+
+describe("Delete", ()=>{
+    test("User can be deleted", async()=>{
+        const email = 'creation@test.com'
+        await User.delete(email)
+        try {
+            await User.fetch("email", email)
+        } catch (error) {
+            expect(error instanceof NotFoundError).toBeTruthy()
+        }
+    })
+})
+
+describe("Testing For Internal Server Errors", ()=>{
+
+    let pool;
+
+    beforeAll(() => {
+        pool = require('../config/database.js');
+        jest.mock('../config/database.js')
+        pool.query.mockImplementation(() => {
+            throw new Error(); 
+        })
+    });
+
+    afterAll(() => {
+        jest.clearAllMocks();
+    });
+
+    test("Create throws internal server error",async ()=>{
+        try {
+            await User.create({password: 'Password123', email: 'creation@test.com', first_name: 'test'})
+        } catch (error) {
+            expect(error instanceof InternalServerError).toBeTruthy()
+        }
+    })
+
+    test("Delete throws internal server error",async ()=>{
+        try {
+            await User.delete('email@test.com')
+        } catch (error) {
+            expect(error instanceof InternalServerError).toBeTruthy()
+        }
+    })
+
+    test("Fetch throws Not Found Error",async ()=>{
+        try {
+            await User.fetch("email",'test-email@test.com')
+        } catch (error) {
+            expect(error instanceof NotFoundError).toBeTruthy()
+        }
+    })
+})
+
--- AiProfolio-api/utilities/authentication.js
+++ AiProfolio-api/utilities/authentication.js
@@ -5,29 +5,7 @@ const {InvalidCredentialsError, NotFoundError} = require('./error.js')
 const User = require('../models/user.js')
 
 async function authenticateJWT(req, res, next){
-
-    const token = req.headers.authorization?.split(' ')[1]
-
-    if (!token) {
-        throw InvalidCredentialsError(message='Token Missing in HTTP Request')
-    }
-
-    try {
-        const decoded = User.verifyJWT(token)
-
-        if (!decoded) {
-            throw InvalidCredentialsError(message='Token Was Not Decoded')
-            }
-
-        const user = await User.fetchById(decoded.id)
-
-        if (!user) {
-            throw NotFoundError(message="User Was Not Found")
-            }
-
-    } catch (error) {
-        throw error
-    }
+    
 }
 
 
