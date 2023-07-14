da834d5890506b4865376f235d1a113cb1ddd549
Alan Salazar <alan.salazar@alansal-ltmyyju.internal.salesforce.com>
2023-07-14 13:04:07 -0700

    Navbar and front end stuff

--- AiProfolio-ui/src/App/App.jsx
+++ AiProfolio-ui/src/App/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Hero from '../Hero/Hero'
- 
+import Navbar from '../Navbar/Navbar'
+import LandingNavbar from '../LandingNavbar/LandingNavbar'
+import { BrowserRouter as Route, BrowserRouter, Routes } from 'react-router-dom';
+import Register from '../Register/Register'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Hero/>
+      <LandingNavbar />
+      <Hero />
+
     </>
   )
 }
