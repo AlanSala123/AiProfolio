fe3ce23df8cfb9b2ffd5ba7d40af977421f791a7
Luis Bravo <lbravo@lbravo-ltmxcuw.internal.salesforce.com>
2023-07-16 19:23:39 -0700

    auth routes

--- AiProfolio-api/routes/auth.js
+++ AiProfolio-api/routes/auth.js
@@ -1,6 +1,25 @@
+const User = require('../models/user.js')
 const app = require('../server.js')
 const express = require('express')
 
 const authRouter = express.Router()
+
+authRouter.post('/register', async (req, res)=>{
+    try {
+        const {user, token} = await User.register(req.body)
+        res.status(200).send({user: user, token: token})
+    } catch (error) {
+        res.status(Number(error.errorCode) || 400).send({error: error.message})
+    }
+})
+
+authRouter.post('/login', async (req, res)=>{
+    try {
+        const {user, newToken} = await User.login(req.body)
+        res.status(200).send({user: user, token: newToken})
+    } catch (error) {
+        res.status(Number(error.errorCode) || 400).send({error: error.message})
+    }
+})
 
 module.exports = authRouter
--- AiProfolio-api/server.js
+++ AiProfolio-api/server.js
@@ -22,14 +22,6 @@ app.use(function (req, res, next) {
   })
   
   /** Generic error handler; anything unhandled goes here. */
-  app.use(function (err, req, res, next) {
-    if (!config.IS_TESTING) console.error(err.stack)
-    const status = err.status || 500
-    const message = err.message
-  
-    return res.status(status).json({
-      error: { message, status },
-    })
-  })
+
 
 module.exports = app
