4617a593efefa25bdc7a10150e9917d4fa757e54
Alan Salazar <alan.salazar@alansal-ltmyyju.internal.salesforce.com>
2023-07-17 15:35:40 -0700

    Added pictures and text to service section

--- AiProfolio-ui/src/OurServices/OurServices.jsx
+++ AiProfolio-ui/src/OurServices/OurServices.jsx
@@ -1,4 +1,9 @@
 import "./OurServices.css"
+import IconSVG from '../assets/IconSVG.jsx';
+import computer from '../assets/Computer.jsx';
+import Customize from '../assets/Customize.jsx';
+
+
 
 export default function OurServices() {
     return (
@@ -7,16 +12,19 @@ export default function OurServices() {
         <h2 className="section-heading">Taking your career to the next level</h2>
         <div className="services-container">
           <div className="service">
-            <img src="path/to/service1.jpg" alt="Service 1" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis, lectus et tincidunt fringilla.</p>
+            {computer}
+            <p className="service-heading">Create</p>
+            <p>All you need to do is drag and drop your resume and we will generate a personal portfolio website</p>
           </div>
           <div className="service">
-            <img src="path/to/service2.jpg" alt="Service 2" />
-            <p>Phasellus aliquam massa quis nisi tincidunt, ac bibendum lacus consequat.</p>
+          {IconSVG}
+            <p className="service-heading">Share</p>
+            <p>Once your portfolio is generated you can share the link with others to view your portfolio</p>
           </div>
           <div className="service">
-            <img src="path/to/service3.jpg" alt="Service 3" />
-            <p>Vestibulum euismod, libero ac convallis volutpat, elit justo dictum sapien.</p>
+            {Customize}
+            <p className="service-heading">Customize</p>
+            <p>You can update anything else in your portfolio that you would like with a simple click of a button</p>
           </div>
         </div>
       </div>
