c4acd159350c83f94861874c9fd9d5025a70a2ab
Bravo-Luis <luisbravo@ucsb.edu>
2023-07-25 07:40:41 -0700

    gpt call

---
+++ AiProfolio-api/models/resume_map.js
@@ -0,0 +1,67 @@
+const resumeTemplateJson = `{
+    "user": {
+      "name": "",
+      "from": "",
+      "email": "",
+      "media-links": []
+    },
+    "summary": "",
+    "contactInformation": {
+      "phone": "",
+      "address": ""
+    },
+    "experiences": [
+      {
+        "experience": "",
+        "location": "",
+        "description": ""
+      }
+    ],
+    "projects": [
+      {
+        "project": "",
+        "description": "",
+        "link": "",
+        "image-url": ""
+      }
+    ],
+    "skills": [],
+    "education": [
+      {
+        "school": "",
+        "location": "",
+        "gpa": "",
+        "major": ""
+      }
+    ],
+    "certifications": [
+      {
+        "name": "",
+        "issuingOrganization": "",
+        "date": ""
+      }
+    ],
+    "awards": [
+      {
+        "title": "",
+        "issuer": "",
+        "date": ""
+      }
+    ],
+    "publications": [
+      {
+        "title": "",
+        "publicationDate": "",
+        "publicationName": ""
+      }
+    ],
+    "volunteerWork": [
+      {
+        "organization": "",
+        "role": "",
+        "dates": ""
+      }
+    ]
+  }`
+
+  module.exports = resumeTemplateJson
