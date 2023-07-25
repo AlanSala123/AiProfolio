8e2e86ebca573ea63477022af0a570547217f904
Alan Salazar <alan.salazar@alansal-ltmyyju.internal.salesforce.com>
2023-07-25 11:11:53 -0700

    fixed some bugs with the login and register and added the landing button animation

--- AiProfolio-ui/src/Hero/Hero.jsx
+++ AiProfolio-ui/src/Hero/Hero.jsx
@@ -1,101 +1,105 @@
 import './Hero.css'
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import Particles from "react-particles";
 import { loadSlim } from "tsparticles-slim";
+import { Link } from 'react-router-dom';
 
-export default function Hero(){
+export default function Hero() {
 
-    const particlesInit = useCallback(async engine => {
+    const [showButton, setShowButton] = useState(false);
+
+    const particlesInit = useCallback(async (engine) => {
         await loadSlim(engine);
     }, []);
 
-    const particlesLoaded = useCallback(async container => {
+    const particlesLoaded = useCallback(async (container) => {
     }, []);
 
-    return(
+    return (
         <header id="hero">
             <Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-                background: {
-                    color: {
-                        value: "#383838",
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    background: {
+                        color: {
+                            value: "#383838",
+                        },
                     },
-                },
-                fpsLimit: 60,
-                interactivity: {
-                    events: {
-                        onClick: {
+                    fpsLimit: 60,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push",
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            push: {
+                                quantity: 2,
+                            },
+                            repulse: {
+                                distance: 100,
+                                duration: 0.4,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#4d935d",
+                        },
+                        links: {
+                            color: "#4d935d",
+                            distance: 150,
                             enable: true,
-                            mode: "push",
+                            opacity: 0.5,
+                            width: 1,
                         },
-                        onHover: {
+                        move: {
+                            direction: "none",
                             enable: true,
-                            mode: "repulse",
+                            outModes: {
+                                default: "bounce",
+                            },
+                            random: false,
+                            speed: 1,
+                            straight: false,
                         },
-                        resize: true,
-                    },
-                    modes: {
-                        push: {
-                            quantity: 2,
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 800,
+                            },
+                            value: 80,
                         },
-                        repulse: {
-                            distance: 100,
-                            duration: 0.4,
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: { min: 1, max: 5 },
                         },
                     },
-                },
-                particles: {
-                    color: {
-                        value: "#4d935d",
-                    },
-                    links: {
-                        color: "#4d935d",
-                        distance: 150,
-                        enable: true,
-                        opacity: 0.5,
-                        width: 1,
-                    },
-                    move: {
-                        direction: "none",
-                        enable: true,
-                        outModes: {
-                            default: "bounce",
-                        },
-                        random: false,
-                        speed: 1,
-                        straight: false,
-                    },
-                    number: {
-                        density: {
-                            enable: true,
-                            area: 800,
-                        },
-                        value: 80,
-                    },
-                    opacity: {
-                        value: 0.5,
-                    },
-                    shape: {
-                        type: "circle",
-                    },
-                    size: {
-                        value: { min: 1, max: 5 },
-                    },
-                },
-                detectRetina: true,
-            }}
-        />
-        <div id="hero-title">
-    <div>
-        <h1 id="hero-ai"> Ai</h1>
-        <h1>Profolio</h1>
-    </div>
-    <p>Professional Portfolio Generator</p>
-</div>
+                    detectRetina: true,
+                }}
+            />
+            <div id="hero-title">
+                <div>
+                    <h1 id="hero-ai"> Ai</h1>
+                    <h1>Profolio</h1>
+                </div>
+                <p>Professional Portfolio Generator</p>
+            </div>
+            <Link to="/register"><button className="get-started-button" >Get Started </button></Link>
         </header>
-        )
+    )
 
 }
--- AiProfolio-ui/src/Login/Login.jsx
+++ AiProfolio-ui/src/Login/Login.jsx
@@ -1,69 +1,54 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './login.css'; 
+import './login.css';
 import { useNavigate } from 'react-router-dom';
 
 export default function Login({ setUser, setToken }) {
-  // State variables for email, password, and error message
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
   const navigate = useNavigate();
 
-  // Function to handle login form submission
   const handleLogin = async (e) => {
-    // Makes it so the page doesnt reload
     e.preventDefault();
     try {
-      // Send a POST request to the login endpoint with email and password
       const response = await axios.post('http://localhost:3001/auth/login', {
         loginForm: {
-          email:email,
+          email: email,
           password: password
         }
       });
-
-      // Extract user and token from the response data
       const { user, token } = response.data;
-
-      // Update the user and token in the parent component
       setUser(user);
       setToken(token);
-
       navigate("/saved-templates")
-
-      // Store the token in localStorage for future use
       localStorage.setItem('token', token);
     } catch (error) {
-      // If there is an error in the response, extract and set the error message
       if (error?.response?.data?.error) {
         const message = error?.response?.data?.error;
         setError(message);
       }
     }
   };
-
-  // Determine if the login button should be disabled based on email and password inputs
   const isLoginButtonDisabled = !(email && password);
-
-  // Determine the styles for the login button based on its disabled state
-const loginButtonStyles = {
-  // If the button is disabled, set the cursor to 'default', otherwise set it to 'pointer'
-  cursor: isLoginButtonDisabled ? 'default' : 'pointer',
-
-  // If the button is disabled, set the background color to '#4d935d', otherwise leave it empty
-  backgroundColor: isLoginButtonDisabled ? '#4d935d' : '',
-
-  // If the button is disabled, apply a contrast filter of 0.75, otherwise leave it empty
-  filter: isLoginButtonDisabled ? 'contrast(0.75)' : '',
-};
+  const loginButtonStyles = {
+    cursor: isLoginButtonDisabled ? 'default' : 'pointer',
+    backgroundColor: isLoginButtonDisabled ? '#4d935d' : '',
+    filter: isLoginButtonDisabled ? 'contrast(0.75)' : '',
+  };
 
 
   return (
     <div className="login">
       <h1>Login</h1>
-      {error ? (<h2 id={error.length >= 22 ? (error.length > 43 ? "error-message-long" : "error-message" ) : "error-message-short"}>{error}</h2>) : (<></>)}
+      <button className="Google">Continue with Google</button>
+      <div className="sep">
+        <div className="l"></div>
+        <span>or</span>
+        <div className="l"></div>
+      </div>
+      {error ? (<h2 id={error.length >= 22 ? (error.length > 43 ? "error-message-long" : "error-message") : "error-message-short"}>{error}</h2>) : (<></>)}
       <form onSubmit={handleLogin} className="loginForm">
         <label>
           <input
@@ -85,7 +70,7 @@ const loginButtonStyles = {
           />
         </label>
         <br />
-        <button 
+        <button
           type="submit"
           className="loginButton"
           disabled={isLoginButtonDisabled}
