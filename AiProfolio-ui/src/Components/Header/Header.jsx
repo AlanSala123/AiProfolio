0914bc72b6ab872a18e2444938b6191398199d59
Luis Bravo <lbravo@lbravo-ltmxcuw.internal.salesforce.com>
2023-07-26 09:52:23 -0700

    testing Header and Skills

--- AiProfolio-ui/src/Components/Header/Header.jsx
+++ AiProfolio-ui/src/Components/Header/Header.jsx
@@ -13,12 +13,12 @@ function Header({header}){
                 justifyContent: "center",
                 alignItems: "center"
             }}>
-                <h1 style={{
+                <h1 id="header-title" style={{
                     fontFamily: header?.foreground?.title?.fontFamily,
                     fontSize: header?.foreground?.title?.fontSize,
                     color: header?.foreground?.title?.fontColor
                 }}> HEADER TITLE</h1>
-                <p style={{
+                <p id="header-subtitle" style={{
                     fontFamily: header?.foreground?.subtitle?.fontFamily,
                     fontSize: header?.foreground?.subtitle?.fontSize,
                     color: header?.foreground?.subtitle?.fontColor
