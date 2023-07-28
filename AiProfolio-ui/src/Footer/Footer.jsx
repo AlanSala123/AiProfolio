8a422507cfc717a53ce357d14a38964807daa51d
Alan Salazar <alan.salazar@alansal-ltmyyju.internal.salesforce.com>
2023-07-28 09:58:18 -0700

    Landing Page finished

--- AiProfolio-ui/src/Footer/Footer.jsx
+++ AiProfolio-ui/src/Footer/Footer.jsx
@@ -1,9 +1,29 @@
 import "./Footer.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHouse, faBolt, faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 export default function Footer() {
     return (
-        <div className="footer">
-            <a href="https://www.google.com/maps/place/Salesforce+Tower/@37.7897484,-122.3998086,17z/data=!3m2!4b1!5s0x808580621f881883:0x28d9d7af1999c538!4m6!3m5!1s0x808580636b2ad2d5:0x272fc1d712ad2912!8m2!3d37.7897442!4d-122.3972337!16s%2Fm%2F0s8wy91?entry=ttu"><img src="https://cdn.discordapp.com/attachments/1123448799692591205/1130924492231807056/Google_Maps_Screenshot.png" /></a>
-        </div>
+        <footer className="footer-container">
+            <div className="waves">
+                <div className="wave" id="wave1"></div>
+                <div className="wave" id="wave2"></div>
+                <div className="wave" id="wave3"></div>
+                <div className="wave" id="wave4"></div>
+            </div>
+            <ul className="social_icon">
+                <li><a href="#"><FontAwesomeIcon icon={faHouse}/></a></li>
+                <li><a href="#"><FontAwesomeIcon icon={faBolt}/></a></li>
+                <li><a href="#"><FontAwesomeIcon icon={faLocationDot}/></a></li>
+                <li><a href="#"><FontAwesomeIcon icon={faEnvelope}/></a></li>
+            </ul>
+            <ul className="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <p> ©2023 AiProfolio | All Rights Reserved </p>
+        </footer>
     )
 }
