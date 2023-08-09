2304dddb3825919dc0c84734b8f88ba8e84fc2fe
Luis <brvoluis@gmail.com>
2023-08-09 10:40:50 -0700

    hedaer

--- AiProfolio-ui/src/Components/Header/Header.jsx
+++ AiProfolio-ui/src/Components/Header/Header.jsx
@@ -21,7 +21,7 @@ function Header({header, data, images}){
     }, [images]);
 
     return (
-        <div id="header" style={{...header?.background, backgroundImage : backgroundImage ? `url(${backgroundImage})` : ""}}>
+        <div id="header" style={{...header?.background, backgroundImage : backgroundImage ? `url(${backgroundImage})` : "", backgroundRepeat :"none", backgroundSize: "cover"}}>
             <h1 id="header-title" style={header?.title}> {data?.user?.name} </h1>
             <p id="header-subtitle" style={header?.subtitle}> {data?.jobAspiration} </p>
         </div> 
